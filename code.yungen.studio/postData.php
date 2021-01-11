384f0368ca455215fa5d1a9996fb0c0bb51bed8a
yungen-lu <70182238+yungen-lu@users.noreply.github.com>
2021-01-11 15:25:13 +0800

    delete unnecessary commit and fix upload

--- code.yungen.studio/postData.php
+++ code.yungen.studio/postData.php
@@ -5,6 +5,9 @@ if (isset($_POST)) {
   $object = json_decode($body, true);  //需要把json轉換為php看得懂的格式
   $input = $object["codeKey"];
   $filename=$object["filenamekey"];  //codescript.js在傳送GET request 時有包含一個filenamekey 以及codeKey值
+  if (!is_dir("upload")) {  //若upload資料夾不存在的話就新增一個
+    mkdir("upload", 0755, true);
+  }
   $fptr = fopen('./upload/'.$filename, "w");
   fwrite($fptr, $input);
   fclose($fptr);
